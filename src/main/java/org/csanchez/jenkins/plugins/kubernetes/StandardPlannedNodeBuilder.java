aae2732eaf676072575c24c5fdd4f56d5cdde21c
jonathannewman <jonathan.newman@puppetlabs.com>
2022-04-29 13:42:47 -0700

    (maint) add logging for timing information for StandardPlannedNodeProvisioner
    Trying to determine bottlenecks in the jenkins environment, and this
    appears like it might be one of them.  This adds logging with timing
    output to understand how long things are really taking.

--- src/main/java/org/csanchez/jenkins/plugins/kubernetes/StandardPlannedNodeBuilder.java
+++ src/main/java/org/csanchez/jenkins/plugins/kubernetes/StandardPlannedNodeBuilder.java
@@ -6,13 +6,19 @@ import hudson.slaves.NodeProvisioner;
 
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * The default {@link PlannedNodeBuilder} implementation, in case there is other registered.
  */
 public class StandardPlannedNodeBuilder extends PlannedNodeBuilder {
+    private static final Logger LOGGER = Logger.getLogger(StandardPlannedNodeBuilder.class.getName());
+
     @Override
     public NodeProvisioner.PlannedNode build() {
+        LOGGER.info("Entering build");
+        long start = System.currentTimeMillis();
         KubernetesCloud cloud = getCloud();
         PodTemplate t = getTemplate();
         CompletableFuture f;
@@ -23,13 +29,19 @@ public class StandardPlannedNodeBuilder extends PlannedNodeBuilder {
                     .podTemplate(cloud.getUnwrappedTemplate(t))
                     .cloud(cloud)
                     .build();
+            LOGGER.info("constructed agent after " + (System.currentTimeMillis() - start) + " milliseconds");
             displayName = agent.getDisplayName();
             f = CompletableFuture.completedFuture(agent);
+            LOGGER.info("future created after " + (System.currentTimeMillis() - start) + " milliseconds");
         } catch (IOException | Descriptor.FormException e) {
+            LOGGER.log(Level.SEVERE, "Failed to create agent", e);
             displayName = null;
             f = new CompletableFuture();
             f.completeExceptionally(e);
         }
-        return new NodeProvisioner.PlannedNode(Util.fixNull(displayName), f, getNumExecutors());
+
+        NodeProvisioner.PlannedNode result = new NodeProvisioner.PlannedNode(Util.fixNull(displayName), f, getNumExecutors());
+        LOGGER.info("Exiting build after " + (System.currentTimeMillis() - start) + " milliseconds");
+        return result;
     }
 }
