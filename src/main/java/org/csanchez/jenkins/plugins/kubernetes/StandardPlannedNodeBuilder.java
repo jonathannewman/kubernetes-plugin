33c2022bfa8c539726cbda021dc007072efee92a
jonathannewman <jonathan.newman@puppetlabs.com>
2022-04-29 14:29:53 -0700

    (maint) move agent creation to separate thread to prevent blocking provisioning.

--- src/main/java/org/csanchez/jenkins/plugins/kubernetes/StandardPlannedNodeBuilder.java
+++ src/main/java/org/csanchez/jenkins/plugins/kubernetes/StandardPlannedNodeBuilder.java
@@ -6,6 +6,9 @@ import hudson.slaves.NodeProvisioner;
 
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,6 +17,7 @@ import java.util.logging.Logger;
  */
 public class StandardPlannedNodeBuilder extends PlannedNodeBuilder {
     private static final Logger LOGGER = Logger.getLogger(StandardPlannedNodeBuilder.class.getName());
+    private static ExecutorService EXECUTOR = Executors.newFixedThreadPool(100);
 
     @Override
     public NodeProvisioner.PlannedNode build() {
@@ -21,26 +25,20 @@ public class StandardPlannedNodeBuilder extends PlannedNodeBuilder {
         long start = System.currentTimeMillis();
         KubernetesCloud cloud = getCloud();
         PodTemplate t = getTemplate();
-        CompletableFuture f;
-        String displayName;
-        try {
+        Future f;
+        f = EXECUTOR.submit(() -> {
+            LOGGER.info("actually creating slave");
             KubernetesSlave agent = KubernetesSlave
                     .builder()
                     .podTemplate(cloud.getUnwrappedTemplate(t))
                     .cloud(cloud)
                     .build();
-            LOGGER.info("constructed agent after " + (System.currentTimeMillis() - start) + " milliseconds");
-            displayName = agent.getDisplayName();
-            f = CompletableFuture.completedFuture(agent);
-            LOGGER.info("future created after " + (System.currentTimeMillis() - start) + " milliseconds");
-        } catch (IOException | Descriptor.FormException e) {
-            LOGGER.log(Level.SEVERE, "Failed to create agent", e);
-            displayName = null;
-            f = new CompletableFuture();
-            f.completeExceptionally(e);
-        }
 
-        NodeProvisioner.PlannedNode result = new NodeProvisioner.PlannedNode(Util.fixNull(displayName), f, getNumExecutors());
+            return agent;
+            }
+        );
+        LOGGER.info("future created after " + (System.currentTimeMillis() - start) + " milliseconds");
+        NodeProvisioner.PlannedNode result = new NodeProvisioner.PlannedNode(Util.fixNull("Kubernetes Agent"), f, getNumExecutors());
         LOGGER.info("Exiting build after " + (System.currentTimeMillis() - start) + " milliseconds");
         return result;
     }
